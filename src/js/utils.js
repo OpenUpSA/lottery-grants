1163f544854cacc282be32bd53e2af3bd2ff0521
“rikusv” <rikusv@gmail.com>
2021-05-07 12:16:33 +0200

    Include R in amounts

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,3 +1,3 @@
 export function formatAmount(value) {
-  return Math.round(Number(value)).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
+  return `R ${Math.round(Number(value)).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}`;
 }
